bf4f0b9bdeb2a71402a5b8b69308d22ef326ecfc
Alien089 <gabri.moratelli@gmail.com>
2023-07-06 00:01:06 +0200

    base menu wip

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -8,4 +9,47 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject m_BaseMenu;
     [SerializeField] private GameObject m_OptionMenu;
     [SerializeField] private GameObject m_CreditsMenu;
+
+    private void Start()
+    {
+        ChangeToBaseMenu();
+    }
+
+    public void Play(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    #region menu navigation
+    public void ChangeToBaseMenu()
+    {
+        m_BaseMenu.SetActive(true);
+        m_OptionMenu.SetActive(false);
+        m_CreditsMenu.SetActive(false);
+    }
+
+    public void ChangeToOptionsMenu()
+    {
+        m_BaseMenu.SetActive(false);
+        m_OptionMenu.SetActive(true);
+        m_CreditsMenu.SetActive(false);
+    }
+
+    public void ChangeToCreditsMenu()
+    {
+        m_BaseMenu.SetActive(false);
+        m_OptionMenu.SetActive(false);
+        m_CreditsMenu.SetActive(true);
+    }
+    #endregion
 }
