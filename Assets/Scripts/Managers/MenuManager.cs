96e570925c4edd93c2dc2c07ff49f1f60964e582
Alien089 <gabri.moratelli@gmail.com>
2023-07-06 15:34:43 +0200

    completed menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -10,9 +12,18 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject m_OptionMenu;
     [SerializeField] private GameObject m_CreditsMenu;
 
+    public AudioMixer AudioMixer;
+    public Slider MasterSlider, MusicSlider, EffectsSlider;
+    private bool activeMusic = true;
+    public Image MusicCheckBox;
+
     private void Start()
     {
         ChangeToBaseMenu();
+        MusicCheckBox.color = Color.cyan;
+        SetSliderSettings(MasterSlider, "VolumeMaster");
+        SetSliderSettings(MusicSlider, "VolumeMusic");
+        SetSliderSettings(EffectsSlider, "VolumeSFX");
     }
 
     public void Play(int indexScene)
@@ -29,6 +40,23 @@ public class MenuManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
     }
+
+    #region audio
+    //public void SetMasterVolume(float volume)
+    //{
+    //    m_AudioMixer.SetFloat("VolumeMaster", volume);
+    //}
+
+    //public void SetMusicVolume(float volume)
+    //{
+    //    m_AudioMixer.SetFloat("VolumeMusic", volume);
+    //}
+
+    //public void SetSFXVolume(float volume)
+    //{
+    //    m_AudioMixer.SetFloat("VolumeSFX", volume);
+    //}
+    #endregion
 
     #region menu navigation
     public void ChangeToBaseMenu()
@@ -52,4 +80,58 @@ public class MenuManager : MonoBehaviour
         m_CreditsMenu.SetActive(true);
     }
     #endregion
+
+
+    /// <summary>
+    /// Changes values of the audio mixer's groups by using sliders
+    /// </summary>
+    public void SetVolume()
+    {
+        SetAudioLevel(MasterSlider, "VolumeMaster", "VolumeMaster");
+        SetAudioLevel(MusicSlider, "VolumeMusic", "VolumeMusic");
+        SetAudioLevel(EffectsSlider, "VolumeSFX", "VolumeSFX");
+    }
+
+    /// <summary>
+    /// toggle music in game and changes color of a chekbox
+    /// </summary>
+    public void ToggleMusic()
+    {
+        if (activeMusic)
+        {
+            AudioMixer.SetFloat("VolumeMaster", 0);
+            MusicCheckBox.color = Color.cyan;
+            activeMusic = !activeMusic;
+            return;
+        }
+        if (!activeMusic)
+        {
+            AudioMixer.SetFloat("VolumeMaster", -80);
+            MusicCheckBox.color = Color.grey;
+            activeMusic = !activeMusic;
+            return;
+        }
+    }
+
+    /// <summary>
+    /// set the slider value to 1
+    /// </summary>
+    /// <param name="slider"></param>
+    /// <param name="key"></param>
+    private void SetSliderSettings(Slider slider, string key)
+    {
+        slider.value = PlayerPrefs.GetFloat(key, 1f);
+    }
+
+    /// <summary>
+    /// saves the audio slider in the PlayerPrefs and perform the actual volume changing
+    /// </summary>
+    /// <param name="slider"></param>
+    /// <param name="key"> string name used as key for the PlayerPrefs</param>
+    /// <param name="groupName">Audio Mixer group name</param>
+    private void SetAudioLevel(Slider slider, string key, string groupName)
+    {
+        AudioMixer.SetFloat(groupName, Mathf.Log10(slider.value) * 20);
+        PlayerPrefs.SetFloat(key, slider.value);
+    }
 }
